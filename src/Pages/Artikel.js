0be1d680b5282032bfe6d7abc4b45e34e14fcfc0
syahrial6 <62192090+syahrial6@users.noreply.github.com>
2023-04-03 13:48:38 +0700

    komentar artikel

--- src/Pages/Artikel.js
+++ src/Pages/Artikel.js
@@ -17,63 +17,63 @@ const Artikel = ({ cards }) => {
   };
 
   return (
-    <Box position="relative">
-      {cards.map((card, index) => (
-        <Box
-          key={index}
-          display={index === currentCardIndex ? 'block' : 'none'}
-          position="relative"
-        >
-          <Image
-            src={card.image}
-            alt={`Card Image ${index}`}
-            objectFit="cover"
-            h="300px"
-            w="100%"
-          />
-          <Box
-            position="absolute"
-            bottom="0"
-            left="0"
-            w="100%"
-            bg="rgba(0, 0, 0, 0.5)"
-            p={4}
-          >
-            <Text fontSize="xl" fontWeight="bold" color="white">
-              {card.title}
-            </Text>
-            <Text fontSize="md" color="white" mt={2}>
-              {card.description}
-            </Text>
-            <Button size="sm" colorScheme="yellow" mt={4}>
-              Lihat Detail
-            </Button>
-          </Box>
-        </Box>
-      ))}
-      <Box
-        position="absolute"
-        top="50%"
-        transform="translateY(-50%)"
-        left="20px"
-        zIndex="2"
-      >
-        <Button onClick={handlePrevClick} colorScheme="yellow">
-          &#8249;
-        </Button>
-      </Box>
-      <Box
-        position="absolute"
-        top="50%"
-        transform="translateY(-50%)"
-        right="20px"
-        zIndex="2"
-      >
-        <Button onClick={handleNextClick} colorScheme="yellow">
-          &#8250;
-        </Button>
-      </Box>
-    </Box>
+//     <Box position="relative">
+//       {cards.map((card, index) => (
+//         <Box
+//           key={index}
+//           display={index === currentCardIndex ? 'block' : 'none'}
+//           position="relative"
+//         >
+//           <Image
+//             src={card.image}
+//             alt={`Card Image ${index}`}
+//             objectFit="cover"
+//             h="300px"
+//             w="100%"
+//           />
+//           <Box
+//             position="absolute"
+//             bottom="0"
+//             left="0"
+//             w="100%"
+//             bg="rgba(0, 0, 0, 0.5)"
+//             p={4}
+//           >
+//             <Text fontSize="xl" fontWeight="bold" color="white">
+//               {card.title}
+//             </Text>
+//             <Text fontSize="md" color="white" mt={2}>
+//               {card.description}
+//             </Text>
+//             <Button size="sm" colorScheme="yellow" mt={4}>
+//               Lihat Detail
+//             </Button>
+//           </Box>
+//         </Box>
+//       ))}
+//       <Box
+//         position="absolute"
+//         top="50%"
+//         transform="translateY(-50%)"
+//         left="20px"
+//         zIndex="2"
+//       >
+//         <Button onClick={handlePrevClick} colorScheme="yellow">
+//           &#8249;
+//         </Button>
+//       </Box>
+//       <Box
+//         position="absolute"
+//         top="50%"
+//         transform="translateY(-50%)"
+//         right="20px"
+//         zIndex="2"
+//       >
+//         <Button onClick={handleNextClick} colorScheme="yellow">
+//           &#8250;
+//         </Button>
+//       </Box>
+//     </Box>
   );
 };
 
